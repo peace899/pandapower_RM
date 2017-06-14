e7057ac9ebd668cacecd1f1db78b72d7f7a21566
Peace L <peacester@gmail.com>
2017-06-14 20:07:23 +0000

    Create rmdextract.py

--- pandapowerRM/rmdextract.py
+++ pandapowerRM/rmdextract.py
@@ -1,6 +1,7 @@
 #Extract data from .rmd file
 # Data in form of spans or sections of line/cable, transformers/minisubs
 
+from __future__ import print_function
 import os, sys
 import re
 import fnmatch
@@ -8,27 +9,31 @@ import fnmatch
 criteria_span = 'Conductor'
 criteria_trfr = 'Trfr'
 
-#select or input path to where .rmd files reside
+#input directory where .rmd files reside
 directory = ''
-#input feeder name
+#input name of feeder
 fdr = ''
 
 def rmd_file(fdr):
-    """Find feeder's .rmd file"""
     for f in os.listdir(directory):
         if fnmatch.fnmatch(f, fdr):
             _rmd_file = os.path.join(directory, f)
         return _rmd_file
 
 def spans(fdr):
-    """Get sections of the feeder"""
-        
-
-
-def transformers(fdr)
-    """Get feeder transformers"""
-    lines = open(rmd_file(fdr), "r" ).readlines()
-    transformers = []
+    spans = []
+    bus_poles = []
+    lines = open(rmd_file(fdr), "r").readlines()
     for line in lines:
-        if re.search( criteria_trfr, line ):
-            print(line, end="")
+        if re.search(criteria_span, line):
+            span = line.split
+            span_length = line.split
+            span_conductor = line.split        
+
+
+def transformers(fdr):
+    lines = open(rmd_file(fdr), "r").readlines()
+    for line in lines:
+        if re.search(criteria_trfr, line):
+        print(line, end="")
+		
